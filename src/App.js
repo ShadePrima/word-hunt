e34acd60497685a4efba8a10064061e8c0df455e
ShadePrima <aleksandrag1984@gmail.com>
2022-09-29 17:21:24 +0300

    add selected lenguage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
+import { Container } from "@material-ui/core";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [word, setWord] = React.useState("");
+  const [meanings, setMeanings] = React.useState([]);
+  const [category, setCategory] = React.useState("en");
+  console.log(category, "app");
+
   const dictionaryApi = async () => {
     try {
       const data = await axios.get(
-        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+        "https://api.dictionaryapi.dev/api/v2/entries/en/dog"
       );
-      console.log(data);
+      setMeanings(data.data);
     } catch (error) {
       console.log(error);
     }
@@ -18,7 +25,18 @@ function App() {
     dictionaryApi();
   }, []);
 
-  return <div>Dictionary</div>;
+  return (
+    <div
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header category={category} setCategory={setCategory} />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import {
+  createTheme,
+  MenuItem,
+  TextField,
+  ThemeProvider,
+} from "@material-ui/core";
+import React from "react";
+import "./Header.css";
+import categories from "../../data/category";
+
+const Header = ({ category, setCategory }) => {
+  console.log(category);
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <div className="header">
+      <span className="title">Word Hunt</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" label="Standard" />
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Select"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            helperText="Please select your currency"
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.label} value={option.label}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
